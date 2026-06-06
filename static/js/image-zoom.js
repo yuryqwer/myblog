fbaadf035b38de56c149e3197c3a644009020acc
yuryqwer <yuryqwer@outlook.com>
2026-06-06 18:09:51 +0800

    support image zoom

---
+++ static/js/image-zoom.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  mediumZoom('.post-content img', {
+    background: 'rgba(0, 0, 0, 0.8)',
+    margin: 32,
+    scrollOffset: 0
+  });
+});
